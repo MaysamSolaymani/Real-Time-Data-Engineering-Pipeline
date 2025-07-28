ca665dcd25b612530785c7d2bcb8df526050a600
Meysam Soleymani <96109623+MaysamSolaymani@users.noreply.github.com>
2025-07-28 11:28:37 +0330

    Update stream_processor.py

--- spark/stream_processor.py
+++ spark/stream_processor.py
@@ -1,1 +1,33 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StringType, TimestampType
 
+if __name__ == '__main__':
+    spark = SparkSession.builder \
+        .appName("KafkaSparkStreaming") \
+        .getOrCreate()
+
+    schema = StructType() \
+        .add("user_id", StringType()) \
+        .add("action", StringType()) \
+        .add("timestamp", StringType())
+
+    df = spark.readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("subscribe", "events") \
+        .option("startingOffsets", "earliest") \
+        .load()
+
+    df_parsed = df.selectExpr("CAST(value AS STRING) as json_str") \
+        .select(from_json(col("json_str"), schema).alias("data")) \
+        .select("data.*")
+
+    clicks = df_parsed.filter(col("action") == "click")
+
+    query = clicks.writeStream \
+        .outputMode("append") \
+        .format("console") \
+        .start()
+
+    query.awaitTermination()
